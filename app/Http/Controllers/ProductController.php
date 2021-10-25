f5d1d594cb1ff4596a08e243b31af1f74ed6a7cf
Kawian <99052760@mydavinci.nl>
2021-10-25 13:14:41 +0200

    Heb de cart functionaliteit toegevoegd

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,11 +4,67 @@ namespace App\Http\Controllers;
 
 use App\Models\Products;
 use Illuminate\Http\Request;
+use App\Cart;
+use App\Models\Categories;
+use App\Models\Order;
 
 class ProductController extends Controller
 {
-    public function getIndex() {
+    public function index() {
+        //Getting all the products that I created in my seeder, from my database
         $products = Products::all();
-        return view('index', ['products' => $products]);
+        $categories = Categories::all();
+
+        // return view('index', [compact('products', $products), compact('categories', $categories)]);
+        return view('index', compact('products', $products), compact('categories', $categories));
+        
+    }
+
+    public function AddToCart(Request $request, $id) {
+        $products = Products::find($id);
+        $cart = new Cart($request);
+        $cart->add($products, $products->id);
+
+        $request->session()->put('cart', $cart);
+        return redirect()->route('index');
+    }
+
+    public function getCart(Request $request) {
+        if (!$request->session()->has('cart')) {
+            return view('shoppingCart', ['products' => null]);
+        }
+        $cart =  new Cart($request);
+        return view('shoppingCart', ['products' => $cart->items, 'totalPrice' => $cart->totalPrice]);
+    }
+
+    public function getCheckout (Request $request) {
+        if (!$request->session()->has('cart')) {
+            return view('ShoppingCart');
+        }
+        $cart = new Cart($request);
+        $total = $cart->totalPrice;
+        return view('checkout', ['total' => $total]);
+    }
+
+    public function postCheckout(Request $request) {
+        $hasCart = $request->session()->has('cart');
+        $cart = new Cart($request);
+        $total = $cart->totalPrice;
+
+        if ( ! $hasCart)
+        {
+            return view('shoppingCart');
+        }
+
+        $order = new Order();
+        //serialize = a way to store the values in an object into a text string format(similar to JSON)
+        $order->cart = serialize($cart);
+        $order->address = $request->input('address');
+        $order->name = $request->input('name');
+        $order->save();
+
+        $request->session()->forget('cart');
+
+        return redirect()->route('index')->with($request->session()->flash('success', 'Payment successful!'));
     }
 }
